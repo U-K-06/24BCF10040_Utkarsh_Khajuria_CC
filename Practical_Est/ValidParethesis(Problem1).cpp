a53ca4484bcfccffd2a9e8041941d1dc97b73278
U-K-06 <utkarsh7khajuria8@gmail.com>
2026-04-25 16:18:49 +0530

    added practical est

---
+++ Practical_Est/ValidParethesis(Problem1).cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        stack<char> s1;
+        unordered_map<char,char> mapping = {
+            {'(',')'},
+            {'[',']'},
+            {'{','}'}
+        };
+        for(char c: s)
+        {
+            if(mapping.containsd(c))
+            {
+                s1.push(i);
+            }
+            if(!s1.isempty() &&  )
+        }
+    }
+};
